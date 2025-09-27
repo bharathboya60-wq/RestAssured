4775364355a1011999eb74e4e07e612d6c537a3a
Bharath <bharathboya60@gmail.com>
2025-09-27 10:45:11 +0530

    PackageFiles added

---
+++ src/main/java/com/Demo/REST/FirstMethod.java
@@ -0,0 +1,10 @@
+package com.Demo.REST;
+//
+//
+//public class FirstMethod {
+//
+//	public static void main(String[] args)
+//	{
+//		RestAssured.baseURI = 
+//	}
+//}
--- src/test/java/com/Demo/REST/Baisc.java
+++ src/test/java/com/Demo/REST/Baisc.java
@@ -2,6 +2,7 @@ package com.Demo.REST;
 //import io.restassured.RestAssured;
 import static io.restassured.RestAssured.*;
 
+import Files.Payload;
 import io.restassured.RestAssured;
 
 
@@ -13,22 +14,7 @@ public class Baisc {
 		
 		given().log().all().queryParam("key", "qaclick123")
 		.header("Content-Type", "application/json")
-		.body("{\r\n"
-				+ "  \"location\": {\r\n"
-				+ "    \"lat\": -38.383494,\r\n"
-				+ "    \"lng\": 33.427362\r\n"
-				+ "  },\r\n"
-				+ "  \"accuracy\": 50,\r\n"
-				+ "  \"name\": \"Frontline house\",\r\n"
-				+ "  \"phone_number\": \"(+91) 983 893 3937\",\r\n"
-				+ "  \"address\": \"29, side layout, cohen 09\",\r\n"
-				+ "  \"types\": [\r\n"
-				+ "    \"shoe park\",\r\n"
-				+ "    \"shop\"\r\n"
-				+ "  ],\r\n"
-				+ "  \"website\": \"http://google.com\",\r\n"
-				+ "  \"language\": \"French-IN\"\r\n"
-				+ "}")
+		.body(Payload.AddPlace())
 		.when().post("/maps/api/place/add/json")
 		.then().log().all().assertThat().statusCode(200);		
 		// TODO Auto-generated method stub
